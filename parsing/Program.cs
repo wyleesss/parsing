2926429235581deac0ef28d0700f5c44011266c9
Koza_jp97@student.itstep.org <Koza_jp97@student.itstep.org>
2023-04-12 15:02:06 +0300

    Add project files.

---
+++ parsing/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+internal static class Program
+{
+    static string FromFloatDecimalToBinary(float number)
+    {
+        char[] ForReverse;
+
+        string StrNumber = number.ToString();
+
+        string StrWholePart = String.Empty;
+        string StrFractionPart = String.Empty;
+
+        string Result = String.Empty;
+
+        if (StrNumber.IndexOf(',') == -1)
+        {
+            StrWholePart = StrNumber;
+        }
+        else
+        {
+            StrWholePart = StrNumber.Substring(0, StrNumber.IndexOf(','));
+            StrFractionPart = String.Concat("0,", StrNumber.Substring(StrNumber.IndexOf(',') + 1));
+        }
+
+        int WholePart = int.Parse(StrWholePart);
+        float FractionPart = float.Parse(StrFractionPart);
+
+        while(WholePart > 0)
+        {
+            Result += (WholePart % 2).ToString();
+            WholePart /= 2;
+        }
+
+        ForReverse = Result.ToCharArray();
+        Array.Reverse(ForReverse);
+        Result = new string(String.Concat(ForReverse.ToString(), ","));
+
+
+    }
+
+    static void Main()
+    {
+        Console.OutputEncoding= Encoding.UTF8;
+
+        string input;
+        float number;
+        ConsoleKeyInfo keyInput = new ConsoleKeyInfo();
+
+        do
+        {
+            Console.Clear();
+            Console.Write("введіть число\n-> ");
+
+            input = new string(Console.ReadLine());
+
+            if (!float.TryParse(input, out number))
+            {
+                Console.WriteLine("\n(!) невірний формат числа");
+                Console.Write("\n\n\nESC - вихід");
+                keyInput = Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"\nрезультат\n=> {FromFloatDecimalToBinary(number)}");
+                Console.Write("\n\nESC - вихід");
+                keyInput = Console.ReadKey();
+            }
+        } 
+        while (keyInput.Key != ConsoleKey.Escape);
+    }
+}
